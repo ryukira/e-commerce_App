cb96b81687dd0dc0c191903cc447622c1c383c91
RB <rullyriel@gmail.com>
2023-02-07 11:16:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/store/rb/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package com.example.store.rb.Prevalent;
+
+import com.example.store.rb.Model.Products;
+import com.example.store.rb.Model.Users;
+
+public class Prevalent {
+
+    public static final   String UserPhoneKey ="UserPhone";
+    public static final String UserPasswordKey="UserPassword";
+    public static Users currentOnlineUser;
+    public static Products currentProduct;
+}
---
+++ app/src/main/java/com/example/store/rb/ViewHolder/ProductViewHolder.java
@@ -0,0 +1,41 @@
+package com.example.store.rb.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.store.rb.Interface.ItemClickListner;
+import com.example.store.rb.R;
+
+public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    public TextView txtProductName, txtProductDescription, txtProductPrice;
+    public ImageView imageView;
+    public ItemClickListner listner;
+    public Button product_details_send_button;
+
+
+    public ProductViewHolder(View itemView)
+    {
+        super(itemView);
+
+        product_details_send_button = (Button) itemView.findViewById(R.id.product_details_send_button);
+
+        imageView = (ImageView) itemView.findViewById(R.id.product_image);
+        txtProductName = (TextView) itemView.findViewById(R.id.product_name);
+        txtProductPrice = (TextView) itemView.findViewById(R.id.product_prices);
+    }
+
+    public void setItemClickListner(ItemClickListner listner)
+    {
+        this.listner = listner;
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        listner.onClick(view, getAdapterPosition(), false);
+    }
+}
